9112ad678528ca2674346b5478f8061f692eed94
Xomano <129993903+Xomano@users.noreply.github.com>
2023-05-30 23:54:07 +0100

    Creating the main code
    This code uses HTML,CSS, JS
    this is to creat a randon card setting red for 'hearts' and 'dimond'. also have a timer that refresh the page every 10 seconds, and, of course a botton that also generate a new card.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
+
+function restartPage(){
+  location.reload();
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  cardGenerator();
+};
+
+function cardGenerator(){
+
+  setTimeout(() => {
+    location.reload();
+  }, 10000);
+
+  const suits = ['♠', '♥', '♦', '♣'];
+  const numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+  const cardTopLeftElement = document.querySelector('.card-top-left');
+  const cardNumberElement = document.querySelector('.card-number');
+  const cardBottomRightElement = document.querySelector('.card-bottom-right');
+  const cardTitleElement = document.querySelector('.title');
+
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  
+  cardTopLeftElement.innerHTML = randomSuit;
+  cardNumberElement.innerHTML = randomNumber;
+  cardBottomRightElement.innerHTML = randomSuit;
+  cardTitleElement.innerHTML = randomSuit;
+  
+  if (randomSuit === '♥' || randomSuit === '♦') {
+    cardTopLeftElement.classList.add('red');
+    cardNumberElement.classList.add('red');
+    cardBottomRightElement.classList.add('red');
+  }
+  
 };
