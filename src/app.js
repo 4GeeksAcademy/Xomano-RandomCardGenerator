c83738f7dc90448dc0c632204ef7c7956d789938
Xomano <129993903+Xomano@users.noreply.github.com>
2023-05-31 19:48:43 +0100

    Fixing issue with timer
    instead of using setTimer on JS now i am using setInterval. also fixed the issue when click on button to generate new card, after a red color all the other cards was red, even the one that should be black.

--- src/app.js
+++ src/app.js
@@ -5,27 +5,27 @@
 // import "./assets/img/rigo-baby.jpg";
 // import "./assets/img/4geeks.ico";
 
-function restartPage(){
-  location.reload();
-}
+// function restartPage(){
+//   location.reload();
+// }
 
 window.onload = function() {
   cardGenerator();
 };
 
+setInterval(() => {
+  cardGenerator();
+}, 10000);
+
 function cardGenerator(){
-
-  setTimeout(() => {
-    location.reload();
-  }, 10000);
-
-  const suits = ['♠', '♥', '♦', '♣'];
-  const numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-
+  
   const cardTopLeftElement = document.querySelector('.card-top-left');
   const cardNumberElement = document.querySelector('.card-number');
   const cardBottomRightElement = document.querySelector('.card-bottom-right');
   const cardTitleElement = document.querySelector('.title');
+
+  const suits = ['♠', '♥', '♦', '♣'];
+  const numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
   const randomSuit = suits[Math.floor(Math.random() * suits.length)];
   const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
@@ -33,12 +33,17 @@ function cardGenerator(){
   cardTopLeftElement.innerHTML = randomSuit;
   cardNumberElement.innerHTML = randomNumber;
   cardBottomRightElement.innerHTML = randomSuit;
-  cardTitleElement.innerHTML = randomSuit;
+  cardTitleElement.innerHTML = randomNumber + ' ' + randomSuit + ' Card generator';
   
   if (randomSuit === '♥' || randomSuit === '♦') {
     cardTopLeftElement.classList.add('red');
     cardNumberElement.classList.add('red');
     cardBottomRightElement.classList.add('red');
+  }
+  else {
+    cardTopLeftElement.classList.remove('red');
+    cardNumberElement.classList.remove('red');
+    cardBottomRightElement.classList.remove('red');
   }
   
 };
